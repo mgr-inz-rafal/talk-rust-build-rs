d46e5fc2de3f5d2b38737a14474276bdc79cd9f5
mgr-inz-rafal <rchabowski@gmail.com>
2019-09-27 10:32:16 +0200

    WIP - build script

---
+++ build.rs
@@ -0,0 +1,29 @@
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+
+fn precalc() -> Vec<u16> {
+    let mut tab = Vec::new();
+    for i in 0..359 {
+        tab.push(((f64::from(i).to_radians().sin() + 1.0) * 50.0).round() as u16);
+    }
+    tab
+}
+
+fn main() -> Result<(), std::io::Error> {
+    let tab = precalc();
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let path = Path::new(&out_dir).join("beret.rs");
+    let mut file = File::create(&path)?;
+    file.write_all("fn dupa() -> Vec<u16> {".as_bytes())?;
+    file.write_all("let tab = vec![".as_bytes())?;
+
+    tab.iter().for_each(|v| {
+        let _ = file.write_all((*v).to_string().as_bytes());
+        let _ = file.write_all(",".as_bytes());
+    });
+
+    file.write_all("];}".as_bytes())?;
+    Ok(())
+}
